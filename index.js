fc318197486b236898b14d9c00ea6af2260c1281
Chargonium <svenvandoorn@icloud.com>
2024-12-22 21:35:53 +0100

    Initial (test) commit!

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log(process.env.WEBHOOK_URL);
+
+fetch(process.env.WEBHOOK_URL, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+        content: "This is an test message!",
+    }),
+}).then((response) => {
+    console.log(response.status);
+});
